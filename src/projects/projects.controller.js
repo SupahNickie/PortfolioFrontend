0f1edab9edda792f3bf13568911e0f7501282a3e
SupahNickie <nicholascase@live.com>
2016-12-29 09:34:57 -0800

    add last of editing powers for projects

--- src/projects/projects.controller.js
+++ src/projects/projects.controller.js
@@ -9,19 +9,24 @@
     var projectsView = this;
 
     projectsView.projects = [];
+    projectsView.images = [];
     projectsView.targets = {
+      "imageIndex": $sce.trustAsResourceUrl("http://127.0.0.1:8080/project/" + Math.floor($routeParams["id"]) + "/images"),
       "imagePost": $sce.trustAsResourceUrl("http://127.0.0.1:8080/project/" + Math.floor($routeParams["id"]) + "/images"),
-      "editProject": $sce.trustAsResourceUrl("http://127.0.0.1:8080/project/" + Math.floor($routeParams["id"]))
+      "imagePut": "http://127.0.0.1:8080/image/",
+      "projectEdit": $sce.trustAsResourceUrl("http://127.0.0.1:8080/project/" + Math.floor($routeParams["id"])),
+      "projectIndex": 'http://127.0.0.1:8080/projects'
     }
 
     init();
 
     projectsView.grabImages = grabImages;
+    projectsView.chooseImage = chooseImage;
 
     function init() {
       $http({
         method: 'GET',
-        url: 'http://127.0.0.1:8080/projects'
+        url: projectsView.targets.projectIndex
       })
       .then(indexProjects);
     }
@@ -37,6 +42,35 @@
 
     function getCurrentProject() {
       return projectsView.projects[Math.floor($routeParams["id"]) - 1];
+    }
+
+    function grabImages() {
+      $http({
+        method: 'GET',
+        url: projectsView.targets.imageIndex,
+        headers: {
+          'portfolio-authorization': window.localStorage['portfolio-token']
+        }
+      })
+      .then(function(response) {
+        projectsView.images = response.data;
+      })
+    }
+
+    function chooseImage(image) {
+      $http({
+        method: 'PUT',
+        url: $sce.trustAsResourceUrl(projectsView.targets.imagePut + image.id),
+        headers: {
+          'portfolio-authorization': window.localStorage['portfolio-token']
+        },
+        data: {
+          'id': image.id,
+          'project_id': image.project_id,
+          'url': image.url,
+          'is_hero_image': true
+        }
+      })
     }
 
   }
